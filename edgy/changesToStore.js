0a70de09c869f98354dc9d185265b85b83d564ed
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2013-11-15 22:11:48 +1100

    Fix whitespace in changesToStore.js

--- edgy/changesToStore.js
+++ edgy/changesToStore.js
@@ -7,17 +7,17 @@
 SnapSerializer.prototype.loadObject = (function loadObject (oldLoadObject) {
     return function (object, model)
     {
-    	var graphLoaded = false;
-    	model.children.forEach(function (child) {
-    		if (child.tag === 'graph') {
-    			object.graphFromJSON(child.contents);
-    			graphLoaded = true;
-    		}
-    	});
-    	if(graphLoaded) {
-    		object.setActiveGraph();
-    	}
-    	return oldLoadObject.call(this, object, model);
+        var graphLoaded = false;
+        model.children.forEach(function (child) {
+            if (child.tag === 'graph') {
+                object.graphFromJSON(child.contents);
+                graphLoaded = true;
+            }
+        });
+        if(graphLoaded) {
+            object.setActiveGraph();
+        }
+        return oldLoadObject.call(this, object, model);
     };
 }(SnapSerializer.prototype.loadObject));
 
