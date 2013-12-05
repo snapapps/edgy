40039aaf30b8974e34f69cfebb2605501cc62d0e
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2013-12-06 00:07:20 +1100

    Hide sprite and simplify looks tab
    The looks tab now only contains the say/think blocks. The sprite is
    now not drawn, but the speech/thought bubbles still show up.

--- edgy/changesToObjects.js
+++ edgy/changesToObjects.js
@@ -271,11 +271,13 @@ SpriteMorph.prototype.init = (function init (oldInit) {
         var retval = oldInit.call(this, globals);
 
         this.name = localize('Graph');
-        this.hide();
 
         return retval;
     };
 }(SpriteMorph.prototype.init));
+
+// Stub this out to hide the actual sprite on the stage.
+SpriteMorph.prototype.drawOn = function() {}
 
 function autoNumericize(x) {
     if(isNumeric(x)) {
@@ -1289,8 +1291,27 @@ SpriteMorph.prototype.blockTemplates = (function blockTemplates (oldBlockTemplat
             );
             blocks.push(button);
             blocks.push(block('getLastfmFriends'));
+        } else if (category === 'looks') {
+            blocks.push(block('doSayFor'));
+            blocks.push(block('bubble'));
+            blocks.push(block('doThinkFor'));
+            blocks.push(block('doThink'));
+            if (this.world().isDevMode) {
+                blocks.push('-');
+                txt = new TextMorph(localize(
+                    'development mode \ndebugging primitives:'
+                ));
+                txt.fontSize = 9;
+                txt.setColor(this.paletteTextColor);
+                blocks.push(txt);
+                blocks.push('-');
+                blocks.push(block('log'));
+                blocks.push(block('alert'));
+            }
+        } else {
+            return blocks.concat(oldBlockTemplates.call(this, category));
         }
-        return blocks.concat(oldBlockTemplates.call(this, category));
+        return blocks;
     };
 }(SpriteMorph.prototype.blockTemplates));
 
