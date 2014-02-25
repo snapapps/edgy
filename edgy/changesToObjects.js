ea5e6bf4679f26acaa4a33d283c53a9f7a64512b
Steven Xu <>
2014-02-25 16:21:38 +1100

    added CounterMorph

--- edgy/changesToObjects.js
+++ edgy/changesToObjects.js
@@ -1429,28 +1429,12 @@ SpriteMorph.prototype.getWordNetDefinition = function(noun) {
     }
 };
 
+/*
+Counter
+*/
 
-SyntaxElementMorph.prototype.labelPart = (function(){
-    var oldLabelPart = SyntaxElementMorph.prototype.labelPart;
-
-    return function (spec) {
-        var part = oldLabelPart.call(this, spec);
-        if(part === undefined){
-            switch (spec) {
-                case '%counter':
-                    part = new CounterMorph('%s', null, 0);
-                    part.addInput();
-                    part.isStatic = true;
-                    part.canBeEmpty = false;
-                    return part;
-            }
-        }else{
-            return part;
-        }
-    }
-})();
-
-function CounterMorph(
+// Menu element.
+function CounterArgMorph(
     slotSpec,
     labelTxt,
     min,
@@ -1473,11 +1457,11 @@ function CounterMorph(
         isTransparent
     );
 }
-CounterMorph.prototype = new MultiArgMorph();
-CounterMorph.prototype.constructor = CounterMorph;
-CounterMorph.uber = MultiArgMorph.prototype;
+CounterArgMorph.prototype = new MultiArgMorph();
+CounterArgMorph.prototype.constructor = CounterArgMorph;
+CounterArgMorph.uber = MultiArgMorph.prototype;
 
-CounterMorph.prototype.addInput = function(contents) {
+CounterArgMorph.prototype.addInput = function(contents) {
     // The arrow morph is the last child.
     if (this.children.length > 2) {
         this.children.splice(this.children.length - 1, 0, this.labelPart(','));
@@ -1485,7 +1469,7 @@ CounterMorph.prototype.addInput = function(contents) {
     MultiArgMorph.prototype.addInput.call(this, contents);
     MultiArgMorph.prototype.addInput.call(this, contents);
 }
-CounterMorph.prototype.removeInput = function(contents) {
+CounterArgMorph.prototype.removeInput = function(contents) {
     MultiArgMorph.prototype.removeInput.call(this, contents);
     MultiArgMorph.prototype.removeInput.call(this, contents);
     if (this.children.length > 2) {
@@ -1494,6 +1478,358 @@ CounterMorph.prototype.removeInput = function(contents) {
     }
 }
 
+/*
+CounterMorph
+*/
+
+CounterMorph.prototype = new BoxMorph();
+CounterMorph.prototype.constructor = CounterMorph;
+CounterMorph.uber = BoxMorph.prototype;
+
+// CounterMorph default settings
+
+CounterMorph.prototype.cellColor =
+    SpriteMorph.prototype.blockColor.lists;
+
+// Result element.
+function CounterMorph(counter, parentCell) {
+    this.init(counter, parentCell);
+}
+
+CounterMorph.prototype.init = function (counter, parentCell) {
+    var myself = this;
+
+    this.counter = counter || new Map();
+    this.counterEntries = [e for(e of counter.entries())];
+    this.start = 1;
+    this.range = 100;
+    this.lastUpdated = Date.now();
+    this.lastCell = null;
+    this.parentCell = parentCell || null; // for circularity detection
+
+    // elements declarations
+    this.label = new StringMorph(
+        localize('size: ') + this.counter.size,
+        SyntaxElementMorph.prototype.fontSize,
+        null,
+        false,
+        false,
+        false,
+        MorphicPreferences.isFlat ? new Point() : new Point(1, 1),
+        new Color(255, 255, 255)
+    );
+    this.label.mouseClickLeft = function () {myself.startIndexMenu(); };
+
+
+    this.frame = new ScrollFrameMorph(null, 10);
+    this.frame.alpha = 0;
+    this.frame.acceptsDrops = false;
+    this.frame.contents.acceptsDrops = false;
+
+    this.handle = new HandleMorph(
+        this,
+        80,
+        70,
+        3,
+        3
+    );
+    this.handle.setExtent(new Point(13, 13));
+
+    this.arrow = new ArrowMorph(
+        'down',
+        SyntaxElementMorph.prototype.fontSize
+    );
+    this.arrow.mouseClickLeft = function () {myself.startIndexMenu(); };
+    this.arrow.setRight(this.handle.right());
+    this.arrow.setBottom(this.handle.top());
+    this.handle.add(this.arrow);
+
+    this.plusButton = new PushButtonMorph(
+        this.counter,
+        'add',
+        '+'
+    );
+    this.plusButton.padding = 0;
+    this.plusButton.edge = 0;
+    this.plusButton.outlineColor = this.color;
+    this.plusButton.drawNew();
+    this.plusButton.fixLayout();
+
+    CounterMorph.uber.init.call(
+        this,
+        SyntaxElementMorph.prototype.rounding,
+        1.000001, // shadow bug in Chrome,
+        new Color(120, 120, 120)
+    );
+
+    this.color = new Color(220, 220, 220);
+    this.isDraggable = true;
+    this.setExtent(new Point(80, 70).multiplyBy(
+        SyntaxElementMorph.prototype.scale
+    ));
+    this.add(this.label);
+    this.add(this.frame);
+    this.add(this.plusButton);
+    this.add(this.handle);
+    this.handle.drawNew();
+    this.update();
+    this.fixLayout();
+};
+
+CounterMorph.prototype.update = function (anyway) {
+    var i, idx, ceil, morphs, cell, cnts, label, button, max,
+        starttime, maxtime = 1000;
+
+    this.frame.contents.children.forEach(function (m) {
+
+        if (m instanceof CellMorph
+                && m.contentsMorph instanceof CounterMorph) {
+            m.contentsMorph.update();
+        }
+    });
+
+    this.updateLength(true);
+
+    // adjust start index to current counterEntries length
+    this.start = Math.max(
+        Math.min(
+            this.start,
+            Math.floor((this.counter.size - 1) / this.range)
+                * this.range + 1
+        ),
+        1
+    );
+
+    // refresh existing cells
+    // highest index shown:
+    max = Math.min(
+        this.start + this.range - 1,
+        this.counter.size
+    );
+
+    // number of morphs available for refreshing
+    ceil = Math.min(
+        (max - this.start + 1) * 3,
+        this.frame.contents.children.length
+    );
+
+    for (i = 0; i < ceil; i += 3) {
+        idx = this.start + (i / 3);
+
+        cell = this.frame.contents.children[i];
+        label = this.frame.contents.children[i + 1];
+        button = this.frame.contents.children[i + 2];
+        cnts = this.counterEntries[idx];
+
+        if (cell.contents !== cnts) {
+            cell.contents = cnts;
+            cell.drawNew();
+            if (this.lastCell) {
+                cell.setLeft(this.lastCell.left());
+            }
+        }
+        this.lastCell = cell;
+
+        if (label.text !== idx.toString()) {
+            label.text = idx.toString();
+            label.drawNew();
+        }
+
+        button.action = idx;
+    }
+
+    // remove excess cells
+    // number of morphs to be shown
+    morphs = (max - this.start + 1) * 3;
+
+    while (this.frame.contents.children.length > morphs) {
+        this.frame.contents.children[morphs].destroy();
+    }
+
+    // add additional cells
+    ceil = morphs; //max * 3;
+    i = this.frame.contents.children.length;
+
+    starttime = Date.now();
+    if (ceil > i + 1) {
+        for (i; i < ceil; i += 3) {
+            if (Date.now() - starttime > maxtime) {
+                this.fixLayout();
+                this.frame.contents.adjustBounds();
+                this.frame.contents.setLeft(this.frame.left());
+                return null;
+            }
+            idx = this.start + (i / 3);
+            label = new StringMorph(
+                idx.toString(),
+                SyntaxElementMorph.prototype.fontSize,
+                null,
+                false,
+                false,
+                false,
+                MorphicPreferences.isFlat ? new Point() : new Point(1, 1),
+                new Color(255, 255, 255)
+            );
+            cell = new CellMorph(
+                this.counterEntries[idx-1],
+                this.cellColor,
+                idx,
+                this.parentCell
+            );
+            button = new PushButtonMorph(
+                this.counterEntries.remove,
+                idx-1,
+                '-',
+                this.counterEntries
+            );
+            button.padding = 1;
+            button.edge = 0;
+            button.corner = 1;
+            button.outlineColor = this.color.darker();
+            button.drawNew();
+            button.fixLayout();
+
+            this.frame.contents.add(cell);
+            if (this.lastCell) {
+                cell.setPosition(this.lastCell.bottomLeft());
+            } else {
+                cell.setTop(this.frame.contents.top());
+            }
+            this.lastCell = cell;
+            label.setCenter(cell.center());
+            label.setRight(cell.left() - 2);
+            this.frame.contents.add(label);
+            this.frame.contents.add(button);
+        }
+    }
+    this.lastCell = null;
+
+    this.fixLayout();
+    this.frame.contents.adjustBounds();
+    this.frame.contents.setLeft(this.frame.left());
+    this.updateLength();
+};
+
+CounterMorph.prototype.updateLength = function (notDone) {
+    this.label.text = localize('length: ') + this.counter.size;
+    if (notDone) {
+        this.label.color = new Color(0, 0, 100);
+    } else {
+        this.label.color = new Color(0, 0, 0);
+    }
+    this.label.drawNew();
+    this.label.setCenter(this.center());
+    this.label.setBottom(this.bottom() - 3);
+};
+
+CounterMorph.prototype.startIndexMenu = function () {
+    var i,
+        range,
+        myself = this,
+        items = Math.ceil(this.counter.size / this.range),
+        menu = new MenuMorph(
+            function (idx) {myself.setStartIndex(idx); },
+            null,
+            myself
+        );
+    menu.addItem('1...', 1);
+    for (i = 1; i < items; i += 1) {
+        range = i * 100 + 1;
+        menu.addItem(range + '...', range);
+    }
+    menu.popUpAtHand(this.world());
+};
+
+CounterMorph.prototype.setStartIndex = function (index) {
+    this.start = index;
+};
+
+CounterMorph.prototype.fixLayout = function () {
+    Morph.prototype.trackChanges = false;
+    if (this.frame) {
+        this.arrangeCells();
+        this.frame.silentSetPosition(this.position().add(3));
+        this.frame.bounds.corner = this.bounds.corner.subtract(new Point(
+            3,
+            17
+        ));
+        this.frame.drawNew();
+        this.frame.contents.adjustBounds();
+    }
+
+    this.label.setCenter(this.center());
+    this.label.setBottom(this.bottom() - 3);
+    this.plusButton.setLeft(this.left() + 3);
+    this.plusButton.setBottom(this.bottom() - 3);
+
+    Morph.prototype.trackChanges = true;
+    this.changed();
+
+    if (this.parent && this.parent.fixLayout) {
+        this.parent.fixLayout();
+    }
+};
+
+CounterMorph.prototype.arrangeCells = function () {
+    var i, cell, label, button, lastCell,
+        end = this.frame.contents.children.length;
+    for (i = 0; i < end; i += 3) {
+        cell = this.frame.contents.children[i];
+        label = this.frame.contents.children[i + 1];
+        button = this.frame.contents.children[i + 2];
+        if (lastCell) {
+            cell.setTop(lastCell.bottom());
+        }
+        if (label) {
+            label.setTop(cell.center().y - label.height() / 2);
+            label.setRight(cell.left() - 2);
+        }
+        if (button) {
+            button.setCenter(cell.center());
+            button.setLeft(cell.right() + 2);
+        }
+        lastCell = cell;
+    }
+    this.frame.contents.adjustBounds();
+};
+
+// CounterMorph hiding/showing:
+
+CounterMorph.prototype.show = function () {
+    CounterMorph.uber.show.call(this);
+    this.frame.contents.adjustBounds();
+};
+
+// CounterMorph drawing:
+
+CounterMorph.prototype.drawNew = function () {
+    WatcherMorph.prototype.drawNew.call(this);
+    this.fixLayout();
+};
+
+// CounterArgMorph input.
+
+SyntaxElementMorph.prototype.labelPart = (function(){
+    var oldLabelPart = SyntaxElementMorph.prototype.labelPart;
+
+    return function (spec) {
+        var part = oldLabelPart.call(this, spec);
+        if(part === undefined){
+            switch (spec) {
+                case '%counter':
+                    part = new CounterArgMorph('%s', null, 0);
+                    part.addInput();
+                    part.isStatic = true;
+                    part.canBeEmpty = false;
+                    return part;
+            }
+        }else{
+            return part;
+        }
+    }
+})();
+
+// Snap's counter methods.
 SpriteMorph.prototype.reportNewCounter = function(elements) {
     var res = new Map();
     for(var i=0;i<elements.contents.length;i+=2){
@@ -1501,6 +1837,26 @@ SpriteMorph.prototype.reportNewCounter = function(elements) {
     }
     return res;
 };
+
+CellMorph.prototype.drawNew = (function() {
+    // Draw snap's results.
+    var oldDrawNew = CellMorph.prototype.drawNew;
+
+    return function () {
+        if (this.contents instanceof Map) {
+            this.contentsMorph = new CounterMorph(this.contents, this);
+            this.contentsMorph.isDraggable = false;
+            this.contents = this.contentsMorph;
+            oldDrawNew.call(this);
+        }else {
+            oldDrawNew.call(this);
+        }
+    };
+})();
+
+/*
+User Menu
+*/
 
 (function() {
     delete SpriteMorph.prototype.categories[SpriteMorph.prototype.categories.indexOf("motion")];
