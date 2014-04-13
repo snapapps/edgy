0f51fb6da8f278f04feff9e2fee94aa659c5b5c0
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2014-04-13 16:44:19 +1000

    Uses spaces instead of tabs in clickstream.js

--- edgy/clickstream.js
+++ edgy/clickstream.js
@@ -2,29 +2,29 @@
 "use strict";
 
 window.ClickstreamLogger = Class.extend({
-	init: function(user) {
-		this.user = user;
-		this.logVersion = "0.1";
-		this.events = [];
-		this.timeBase = new Date().getTime();
+    init: function(user) {
+        this.user = user;
+        this.logVersion = "0.1";
+        this.events = [];
+        this.timeBase = new Date().getTime();
 
-		var ids = {};
-		this.nextID = function(what) {
-			if(ids[what] === undefined) {
-				ids[what] = 0;
-			}
+        var ids = {};
+        this.nextID = function(what) {
+            if(ids[what] === undefined) {
+                ids[what] = 0;
+            }
 
-			return ids[what]++;
-		}
-	},
-	log: function(type, data) {
-		var dt = new Date().getTime() - this.timeBase;
-		this.events.push({
-			time: dt,
-			type: type,
-			data: data
-		});
-	}
+            return ids[what]++;
+        }
+    },
+    log: function(type, data) {
+        var dt = new Date().getTime() - this.timeBase;
+        this.events.push({
+            time: dt,
+            type: type,
+            data: data
+        });
+    }
 });
 
 }());
